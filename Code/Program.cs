0c86d98d3cbae74e734f7ec479d4f4d4ec4b667c
Nano1412 <nano.khongsri@gmail.com>
2023-05-16 06:54:33 +0700

    Outbreak, Vaccinate, Lock down

--- Code/Program.cs
+++ Code/Program.cs
@@ -5,13 +5,11 @@ class Program
   struct Town
   {
     public string name;
-    public int id;
     public int[] adjacentTownID;
     public int covidRate;
-    public Town(string name,int id, int[] adjacentTownID)
+    public Town(string name, int[] adjacentTownID)
     {
       this.name = name;
-      this.id = id;
       this.adjacentTownID = adjacentTownID;
       this.covidRate = 0;
     }
@@ -21,9 +19,55 @@ class Program
     int numOfTown = int.Parse(Console.ReadLine());
     Town[] townArray = new Town[numOfTown];
     Input(ref townArray);
-    TestPrint(townArray);
+    // TestPrint(townArray);
+    CovidStatus(townArray);
 
 
+    bool inLoop = true;
+    int cityReportID = 0;
+    int selfInfect = 0;
+    int adjacentInfect = 0;
+    while (inLoop)
+    {
+      string cityReport = Console.ReadLine();
+      if (cityReport == "Outbreak" || cityReport == "Vaccinate" || cityReport == "Lock down")
+      {
+        cityReportID = int.Parse(Console.ReadLine());
+      }
+      switch (cityReport)
+      {
+        case "Outbreak":
+          selfInfect = 2;
+          adjacentInfect = 1;
+          break;
+        case "Vaccinate":
+          selfInfect = -4;
+          adjacentInfect = 0;
+          break;
+        case "Lock down":
+          selfInfect = -1;
+          adjacentInfect = -1;
+          break;
+        case "Spread":
+          // call spread function here
+          selfInfect = 0;
+          adjacentInfect = 0;
+          break;
+        case "Exit":
+          inLoop = false;
+          break;
+        default:
+          Console.WriteLine("Invalid");
+          selfInfect = 0;
+          adjacentInfect = 0;
+          break;
+      }
+      if (cityReport == "Outbreak" || cityReport == "Vaccinate" || cityReport == "Lock down")
+      {
+        CovidUpdate(ref townArray, cityReportID, selfInfect, adjacentInfect);
+        CovidStatus(townArray);
+      }
+    }
   }
 
   static void Input(ref Town[] townArray)
@@ -38,7 +82,7 @@ class Program
       for (int currentAdjacentTown = 0; currentAdjacentTown < adjacentTownID.Length; currentAdjacentTown++)
       {
         addAdjacentTownID = int.Parse(Console.ReadLine());
-        
+
         if (addAdjacentTownID > townArray.Length || addAdjacentTownID == thisTownID || addAdjacentTownID < 0 || adjacentTownID.Contains(addAdjacentTownID))
         {
           Console.WriteLine("Invalid ID");
@@ -49,25 +93,52 @@ class Program
           adjacentTownID[currentAdjacentTown] = addAdjacentTownID;
         }
       }
-      townArray[thisTownID] = new Town(name,thisTownID, adjacentTownID);
+      townArray[thisTownID] = new Town(name, adjacentTownID);
     }
-
   }
-  static void SetupArray(ref int[] array){
-    for(int i = 0; i < array.Length;i++){
+  static void SetupArray(ref int[] array)
+  {
+    for (int i = 0; i < array.Length; i++)
+    {
       array[i] = -1;
     }
   }
 
-  static void TestPrint(Town[] town){
-    for(int thisTown = 0; thisTown < town.Length;thisTown++){
-      Console.WriteLine("Name: {0} ID: {1}",town[thisTown].name,town[thisTown].id);
+  static void CovidStatus(Town[] town)
+  {
+    for (int thisTownID = 0; thisTownID < town.Length; thisTownID++)
+    {
+      Console.WriteLine("{0} {1} {2}", thisTownID, town[thisTownID].name, town[thisTownID].covidRate);
+    }
+  }
+
+  static void TestPrint(Town[] town)
+  {
+    for (int thisTownID = 0; thisTownID < town.Length; thisTownID++)
+    {
+      Console.WriteLine("Name: {0} ID: {1}", town[thisTownID].name, thisTownID);
       Console.WriteLine("adjacent town:");
-      for(int currentAdjacentTown = 0; currentAdjacentTown < town[thisTown].adjacentTownID.Length; currentAdjacentTown++){
-        Console.WriteLine("{0} ID: {1} Name:{2}",currentAdjacentTown,town[thisTown].adjacentTownID[currentAdjacentTown],town[town[thisTown].adjacentTownID[currentAdjacentTown]].name);
+      for (int currentAdjacentTown = 0; currentAdjacentTown < town[thisTownID].adjacentTownID.Length; currentAdjacentTown++)
+      {
+        int currentAdjacentTownID = town[thisTownID].adjacentTownID[currentAdjacentTown];
+        Console.WriteLine("{0} ID: {1} Name:{2}", currentAdjacentTown, currentAdjacentTownID, town[currentAdjacentTownID].name);
       }
       Console.WriteLine("---------------------------");
     }
+  }
 
+  static void CovidUpdate(ref Town[] townArray, int cityReportID, int selfInfect, int adjacentInfect)
+  {
+    townArray[cityReportID].covidRate += selfInfect;
+    Clamp(ref townArray[cityReportID].covidRate, 0, 3);
+    for (int currentAdjacentTownID = 0; currentAdjacentTownID < townArray[cityReportID].adjacentTownID.Length; currentAdjacentTownID++)
+    {
+      townArray[townArray[cityReportID].adjacentTownID[currentAdjacentTownID]].covidRate += adjacentInfect;
+      Clamp(ref townArray[townArray[cityReportID].adjacentTownID[currentAdjacentTownID]].covidRate, 0, 3);
+    }
+  }
+  static void Clamp(ref int num, int min, int max)
+  {
+    num = (num < min) ? min : (num > max) ? max : num;
   }
 }
